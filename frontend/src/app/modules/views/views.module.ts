1682f973a1f21ca89e73c90f4732ea3955e5ea4c
deepaksama <deepak.sama@gmail.com>
2023-05-08 00:37:32 -0600

    Removed unwanted components

--- frontend/src/app/modules/views/views.module.ts
+++ frontend/src/app/modules/views/views.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MbtTestingComponent } from '../../views/mbt-testing/mbt-testing.component';
 import { SettingsComponent } from '../../views/settings/settings.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedComponentsModule } from '../shared-components/shared-components.module';
 
 @NgModule({
   declarations: [
-    MbtTestingComponent,
     SettingsComponent
   ],
   imports: [
